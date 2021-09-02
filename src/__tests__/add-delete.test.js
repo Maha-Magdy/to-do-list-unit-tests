a30282f4b079e6646fc1ce985f7c66588b246e86
ixboy <ismaelixboy@gmail.com>
2021-09-02 17:56:57 +0100

    grouped  tests

--- src/__tests__/add-delete.test.js
+++ src/__tests__/add-delete.test.js
@@ -2,24 +2,27 @@
  * @jest-environment jsdom
  */
 
-import { addNewTaskToList, deleteItem } from "../update-to-do-list";
-import { expect } from "@jest/globals";
+import { addNewTaskToList, deleteItem } from '../update-to-do-list';
 
-jest.mock("../handle-storage.js");
+import { expect } from '@jest/globals';
 
-document.body.innerHTML = `<input type="text" placeholder="Add to your list..." id="new-task">`;
+jest.mock('../handle-storage.js');
+
+document.body.innerHTML = '<input type="text" placeholder="Add to your list..." id="new-task">';
 
 let toDoTasks = [];
 
-test("should add the new task into the list of to do list", () => {
-  const newTask = document.getElementById("new-task");
-  newTask.value = `Wash the dishes`;
-  toDoTasks = addNewTaskToList();
+describe('My To-Do List', () => {
+  test('should add the new task into the list of to do list', () => {
+    const newTask = document.getElementById('new-task');
+    newTask.value = 'Wash the dishes';
+    toDoTasks = addNewTaskToList();
 
-  expect(toDoTasks).toHaveLength(1);
-});
+    expect(toDoTasks).toHaveLength(1);
+  });
 
-test('should delete the item when passed the index of it', () => {
-  toDoTasks = deleteItem(0);
-  expect(toDoTasks).toHaveLength(0);
+  test('should delete the item when passed the index of it', () => {
+    toDoTasks = deleteItem(0);
+    expect(toDoTasks).toHaveLength(0);
+  });
 });
